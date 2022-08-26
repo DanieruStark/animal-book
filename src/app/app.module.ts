2767c8c1c47dcfdec85323e15eb2b10878f80013
Daniel Santana <danielmaster19@gmail.com>
2022-08-26 05:45:25 -0300

    criado sistema de autenticação de login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,20 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeModule } from './home/home.module';
+import { MensagemComponent } from './componentes/mensagem/mensagem.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MensagemComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
