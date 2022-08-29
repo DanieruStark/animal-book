c5facd8a6fae9854b22485ba36001cc934f267b7
Daniel Santana <danielmaster19@gmail.com>
2022-08-29 05:45:16 -0300

    criado a grade de cartões dos animais

--- src/app/animais/lista-animais/lista-animais.component.ts
+++ src/app/animais/lista-animais/lista-animais.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, switchMap } from 'rxjs';
+import { UsuarioService } from 'src/app/autenticacao/usuario/usuario.service';
+import { Animais } from '../animais';
+import { AnimaisService } from '../animais.service';
 
 @Component({
   selector: 'app-lista-animais',
@@ -7,9 +11,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaAnimaisComponent implements OnInit {
 
-  constructor() { }
+  animais$!: Observable<Animais>;
+
+  constructor(private usuaruioService: UsuarioService, private animaisService: AnimaisService) { }
 
   ngOnInit(): void {
+    this.animais$ = this.usuaruioService.retornaUsuario().pipe(
+      switchMap((usuario) => {
+        const userName = usuario.name ?? '';
+        return this.animaisService.listaDoUsuario(userName);
+      })
+    );
   }
 
 }
